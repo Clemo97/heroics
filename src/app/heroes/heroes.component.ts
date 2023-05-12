7c6c80c6ef2f39ab1e2a25dfb7977b8b9df55aa9
Clemo97 <lumumbaclement@gmail.com>
2023-05-13 01:01:25 +0300

    hero angular app

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+
+export class HeroesComponent {
+
+
+  constructor(private heroService: HeroService) { }
+
+  heroes: Hero[] = [];
+
+  selectedHero?: Hero;
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+}
